f801ae57f30b232bfd401948d2f917c9e29b1876
Marek Jelen <marek@jelen.biz>
2011-10-01 15:33:55 +0200

    Bump version

--- lib/noderb/modules/http/version.rb
+++ lib/noderb/modules/http/version.rb
@@ -2,7 +2,7 @@ module NodeRb
   module Modules
     module Http
       
-      VERSION = "0.0.3" unless const_defined?(:VERSION)
+      VERSION = "0.0.4" unless const_defined?(:VERSION)
     
     end
   end
